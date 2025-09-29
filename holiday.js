57d182701c8199717c2db150dfb9a982aff16b17
Joacim <18618554+joacimstrandvide@users.noreply.github.com>
2025-09-29 14:11:41 +0200

    snabb fix

--- holiday.js
+++ holiday.js
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-/* Innehållsförteckning 
-API = https://date.nager.at/Api
-1. Pulic Holidays Fetch
-
-*/
-
-// 1. Public Holidays Fetch
-
-// Get textfield and input button with querySelector and assign them to respective buttons
-let holidayText = document.querySelector("#holidayText");
-let holidayBtn = document.querySelector("#getHoliday");
-
-// Add a event listener to the input button, it listens for a "click" event and executes a function when that happens
-holidayBtn.addEventListener("click", searchHolidays);
-
-// Search holiday function, function that is called by holidayBtn
-function searchHolidays() {
-  // make a GET fetch request to the api with a query
-  fetch(`https://date.nager.at/api/v3/PublicHolidays/2022/${holidayText.value}`)
-    .then((response) => {
-      // Error message
-      if (!response.ok) {
-        throw Error("Error!");
-      }
-      // return json
-      return response.json();
-    })
-    // Handle the data we have received
-    .then((data) => {
-      // ForEach, creates a header for each item in the array
-      data.forEach((country) => {
-        let newElement = document.createElement("h3");
-        newElement.innerHTML = `
-        ${country.localName}<br> <strong>${country.date}</strong>
-        `;
-
-        //Append the newly created element to holidayBox
-        let holidayBox = document.querySelector("#holidayBox");
-        holidayBox.appendChild(newElement);
-      });
-    })
-    .catch((error) => {
-      // Catch to handle errors
-      console.log(error);
-    });
-}
-=======
 /*
   API = https://date.nager.at/Api
   1. Public Holidays Fetch
@@ -105,4 +57,3 @@ function searchHolidays() {
             console.log(error)
         })
 }
->>>>>>> 0a69a26 (Små uppdateringar)
--- index.js
+++ index.js
@@ -1,68 +1,3 @@
-<<<<<<< HEAD
-// Innehållsförteckning
-/*
-API = https://date.nager.at/Api
-1. Country Information Fetch
-
-*/
-
-// 1. Fetch
-// Assign the inputs to each respective variable with querySelector
-let countryBtn = document.querySelector("#getCountry");
-let countryText = document.querySelector("#searchCountry");
-
-// Add a eventlistener to the button variable to listen for clicks and execute a function
-countryBtn.addEventListener("click", retriveCountry);
-
-// Country Information Fetch, function that is called with countryBtn
-function retriveCountry() {
-  //Fetch the API in question with a query
-  fetch(`https://date.nager.at/api/v3/CountryInfo/${countryText.value}`)
-    .then((response) => {
-      // Error message handling
-      if (!response.ok) {
-        throw Error("Error!");
-      }
-      // return json
-      return response.json();
-    })
-    // Write to the div with resultBox as id all the different object information
-    .then((data) => {
-      // Write the country specified fetch data to the div
-      document.querySelector("#resultBox").innerHTML = `
-      <h3>Name: <strong>${data.commonName}</strong><br>
-      Official Name: <strong>${data.officialName}</strong><br>
-      Country Code: <strong>${data.countryCode}</strong><br>
-      Geographic Region: <strong>${data.region}</strong><br>
-      Flag: <br> <br>
-      <img src="img/${data.commonName}.jpg">
-      </h3>
-      <h2>-- Neighbors --</h2>
-      `;
-
-      // Loop through each neighboor and create a header for each
-      for (i = 0; i < data.borders.length; i++) {
-        let newElement = document.createElement("h3");
-        newElement.innerHTML = `
-        Name: <strong>${data.borders[i].commonName}</strong><br>
-        Official Name: <strong>${data.borders[i].officialName}</strong><br>
-        Country Code: <strong>${data.borders[i].countryCode}</strong><br>
-        Geographic Region: <strong>${data.borders[i].region}</strong><br>
-        Flag: <br> <br>
-        <img src="img/${data.borders[i].commonName}.jpg">
-       `;
-
-        // append the content of newElement to the div
-        let resultBox = document.querySelector("#resultBox");
-        resultBox.appendChild(newElement);
-      }
-    })
-    .catch((error) => {
-      // Catch to handle errors
-      console.log(error);
-    });
-}
-=======
 /*
 API = https://date.nager.at/Api
 1. Country Information Fetch
@@ -133,4 +68,3 @@ function retriveCountry() {
             console.log(error)
         })
 }
->>>>>>> 0a69a26 (Små uppdateringar)
